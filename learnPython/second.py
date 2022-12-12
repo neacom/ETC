85afeda7970eec47c961c2bf6c331ff914e9998f
neacom <akuntakterdaftar@gmail.com>
2022-12-12 14:15:31 +0700

    Second Code with Python

---
+++ learnPython/second.py
@@ -0,0 +1,23 @@
+#Input, Output, Tipe data, Operator, Variable, Statement, Expression dan Komentar
+
+print("Nea")
+#print merupakan perintah unuk menampilkan sesuatu ke media output
+#kurung adalah prgram yang di jalankan
+#tanda petik untuk menandakan string yang
+
+#data yang tersimpan dalam komputer memiliki jenis atau type
+#dalam python ada beberapa type data dasar yang sering di gunakan dalam pemrograman
+#yaitu:
+#Sring: karakter atau text
+#Integer: bilangan bulat seperti 7
+#Float: bilangan pecahan seperti 7.6
+#Boolean: logika benar atau salah yaitu True dan False
+
+# \ mengabakan pindah baris seingga bisa menulis secara lebih dari satu baris namun hanya di anggap sebagai satu baris
+# \n untuk berpindah baris
+# \t untuk tab
+# \\ agari backslash tidak di anggap sebagai escape character
+# \' agar ' tidak dianggap sebagai tanda pembuka atau penutup string
+# \" agar " tidak dianggap sebagai tanda pembuka atau penutup string
+print("Ini \
+dua baris tapi dianggap satu baris")
